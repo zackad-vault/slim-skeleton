9cf2f1ee0b04aad230ba1d5f761940a1952f4609
zackad <zackad.dev@gmail.com>
2016-12-27 09:37:04 +0700

    configuration file

---
+++ src/settings.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'settings' => [
+        'displayErrorDetails' => true, // set to false in production
+        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+
+        // Renderer settings
+        'twig' => [
+            'template_path' => __DIR__ . '/../templates/',
+            'debug' => true, // set to false in production
+        ],
+
+        // Monolog settings
+        'logger' => [
+            'name' => 'slim-app',
+            'path' => __DIR__ . '/../logs/app.log',
+            'level' => \Monolog\Logger::DEBUG,
+        ],
+    ],
+];
