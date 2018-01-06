776baee1d83a87965664ee003549cd7f4a4d1381
zackad <zackad.dev@gmail.com>
2018-01-06 13:08:14 +0700

    using environmental as configuration value

--- src/settings.php
+++ src/settings.php
@@ -1,20 +1,30 @@
 <?php
+
+$dotenv = new Dotenv\Dotenv(dirname(__DIR__));
+$dotenv->load();
+$dotenv->required([
+    'DISPLAY_ERROR_DETAILS',
+    'LOGGER_PATH',
+    'TWIG_DEBUG',
+    'TWIG_TEMPLATE_PATH',
+]);
+
 return [
     'settings' => [
-        'displayErrorDetails' => true, // set to false in production
-        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+        'displayErrorDetails' => getenv('DISPLAY_ERROR_DETAILS'), // set to false in production
+        'addContentLengthHeader' => getenv('ADD_CONTENT_LENGTH_HEADER'), // Allow the web server to send the content-length header
 
         // Renderer settings
         'twig' => [
-            'template_path' => __DIR__ . '/../templates/',
-            'debug' => true, // set to false in production
+            'template_path' => getenv('TWIG_TEMPLATE_PATH'),
+            'debug' => getenv('TWIG_DEBUG'), // set to false in production
         ],
 
         // Monolog settings
         'logger' => [
-            'name' => 'slim-app',
-            'path' => __DIR__ . '/../logs/app.log',
-            'level' => \Monolog\Logger::DEBUG,
+            'name' => getenv('LOGGER_NAME'),
+            'path' => getenv('LOGGER_PATH'),
+            'level' => getenv('LOGGER_LEVEL'),
         ],
     ],
 ];
